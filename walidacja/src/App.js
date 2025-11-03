7fefa8bcb123cfd736d0d7c407a1fed83b707669
fedoseevm <maksim.fedoseev122@gmail.com>
2025-11-03 14:21:08 +0100

    Add walidacja

---
+++ walidacja/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Props from './WalidacjaDuzy.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Props propArray={[2,4,6]} propBool={false} />
+    </div>
+  );
+}
+
+export default App;
