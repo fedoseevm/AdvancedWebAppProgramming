dd8ffb2f6dde4c0cbd2b088b9c7dd0950dcc995f
fedoseevm <maksim.fedoseev122@gmail.com>
2025-11-06 11:11:17 +0100

    Update biblioteka

--- biblioteka - komunikacja ze strona/src/Books.js
+++ biblioteka - komunikacja ze strona/src/Books.js
@@ -8,18 +8,18 @@ class Books extends Component {
         super(props);
         this.state = {
             ksiazki:[
-                {tytul: "Hobbit", autor: "J. R. Tolkien", stron: "455"},
-                {tytul: "Rok 198", autor: "G. Orwell", stron: "300"},
-                {tytul: "Wladca Pierscieni", autor: "J. R. Tolkien", stron: "669"},
-                {tytul: "Stary czlowkiek i morze", autor: "E. Hemingway", stron: "226"}
+                {id: 101, tytul: "Hobbit", autor: "J. R. Tolkien", stron: "455"},
+                {id: 102 ,tytul: "Rok 198", autor: "G. Orwell", stron: "300"},
+                {id: 103, tytul: "Wladca Pierscieni", autor: "J. R. Tolkien", stron: "669"},
+                {id: 104, tytul: "Stary czlowkiek i morze", autor: "E. Hemingway", stron: "226"}
             ]
             };
         }
     updateBooks = ()=>{
         this.setState({
             ksiazki:[
-                {tytul: "Hobbit", autor: "J. R. Tolkien", stron: "455"},
-                {tytul: "Wladca Pierscieni", autor: "J. R. Tolkien", stron: "669"},
+                {id:101, tytul: "Hobbit", autor: "J. R. Tolkien", stron: "455"},
+                {id:103, tytul: "Wladca Pierscieni", autor: "J. R. Tolkien", stron: "669"},
             ]  
         });
     };
@@ -30,19 +30,36 @@ class Books extends Component {
                 ksiazki:
                 [
                     ...prevState.ksiazki,
-                    {tytul: "Nowy wspanialy swiat", autor: "A. Huxley", stron: "255"}
+                    {id: prevState.ksiazki.length+1, 
+                     tytul: "Nowy wspanialy swiat", 
+                     autor: "A. Huxley", 
+                     stron: "255"}
                 ]
             }
         ));
     };
+
+    deleteSingleBook = id => {
+        this.setState({
+            ksiazki: this.state.ksiazki.filter(function(book){
+                return book.id !== id;
+            }
+            )
+        });
+    };
+
     render (){
         return(
             <div>
                 <Header />
                 <button onClick={this.updateBooks}>Pozostaw tylko ksiazki Tolkiena</button>
                 <button onClick={this.addNewBook}>Dodaj nowa ksiazke</button>
-                {this.state.ksiazki.map((book,i)=> (
-                    <BookDescription book ={book} />
+                {this.state.ksiazki.map((book)=> (
+                    <BookDescription 
+                     key = {book.id} 
+                     book ={book}
+                     deleteBook={this.deleteSingleBook}
+                    />
                 )
                 )}
                 <Footer />
--- biblioteka - komunikacja ze strona/src/components/BookDescription.js
+++ biblioteka - komunikacja ze strona/src/components/BookDescription.js
@@ -1,12 +1,25 @@
+import PropTypes from "prop-types"
 const BookDescription = (props) => {
     return (
         <div>
             <h2>Tytul: {props.book.tytul}</h2>
             <h4>Autor: {props.book.autor}</h4>
             <h6>{props.book.stron} stron</h6>
+            <button onClick={()=>props.deleteBook(props.book.id)}>Usun ksiazke</button>
             <hr/>
         </div>
-    )
-}
+    );
+};
+
+BookDescription.propTypes = {
+    book: PropTypes.object.isRequired,
+    deleteBook: PropTypes.func
+};
+
+BookDescription.defaultProps = {
+    book: {
+        tytul: "Tytul ksiazki", autor: "Autor ksiazki", stron: "Liczba stron"
+    }
+};
 
 export default BookDescription;
