2b07ff5bc4d5a9c0772cd689376bcdd2bb59f7b2
fedoseevm <maksim.fedoseev122@gmail.com>
2025-12-17 22:14:51 +0100

    Add zadanie domowe

---
+++ bootstrap i obsluga zdarzen/rownanie kwadratowe/src/komponenty/Footer.js
@@ -0,0 +1,10 @@
+function Footer(props) {
+    return (
+        <footer>
+            <h4>Pomyślnego rozwiązywania {props.rodzajZadania}!</h4>
+        </footer>
+    )
+}
+
+
+export default Footer;
