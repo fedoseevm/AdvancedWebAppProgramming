515165939d5a708bafed50f4b4f219775e3a763d
fedoseevm <maksim.fedoseev122@gmail.com>
2026-02-12 12:28:14 +0100

    Small changes

--- pole wejsciowe formularza/wycieczki/src/strony/Checkout.js
+++ pole wejsciowe formularza/wycieczki/src/strony/Checkout.js
@@ -8,16 +8,16 @@ function Checkout() {
     const sposobWyzywienia = ["własne", "śniadanie", "pełne"];
 
     const miasta = [
-        {id: "--", name: "---"},
-        {id: "wrs", name: "Warszawa"},
-        {id: "krk", name: "Kraków"},
-        {id: "Lbl", name: "Lublin"}
+        { name: "---" },
+        { name: "Warszawa" },
+        { name: "Kraków" },
+        { name: "Lublin" }
     ];
 
     const [daneOsobowe, setDaneOsobowe] = React.useState({
         miejscowosc: "",
         iloscOsob: null,
-        dojazd: false,
+        dojazdWlasny: false,
         wyzywienie: "",
         uwagi: "",
         email: "",
@@ -26,7 +26,7 @@ function Checkout() {
 
     const changeHandler = event => {
         let inputName = event.target.name;
-        let inputValue = event.target.type === "checkbox" ? event.target.cheched : event.target.value;
+        let inputValue = event.target.type === "checkbox" ? (event.target.checked ? true : false) : event.target.value;
         setDaneOsobowe((prevState) => ({
             ...prevState,
             [inputName]: inputValue
@@ -39,60 +39,114 @@ function Checkout() {
         console.log(daneOsobowe);
     };
 
+    const obliczWartosc = () => {
+        daneOsobowe.wartoscWycieczki = 0;
+        const cenaMiasta = 550;
+        const cenaDojazdu = 60;
+        const cenaSniadania = 50;
+        const cenaObiadu = 75;
+
+        if (daneOsobowe.miejscowosc !== "---") {
+            daneOsobowe.wartoscWycieczki += cenaMiasta;
+        }
+
+        if (!daneOsobowe.dojazdWlasny){
+            daneOsobowe.wartoscWycieczki += cenaDojazdu;
+        }
+
+        if (daneOsobowe.wyzywienie === "śniadanie") {
+            daneOsobowe.wartoscWycieczki += cenaSniadania;
+        }
+        else if (daneOsobowe.wyzywienie === "pełne") {
+            daneOsobowe.wartoscWycieczki += cenaObiadu + cenaSniadania*2;
+        }
+
+        if (isNaN(daneOsobowe.iloscOsob) || daneOsobowe.iloscOsob <= 0) {
+            daneOsobowe.wartoscWycieczki *= daneOsobowe.iloscOsob;
+        }
+    }
+
 
     return (
-        <form onSubmit={submitHandler}>
-            <MySelect
-                name="miejscowosc"
-                label="Proszę wybrać miejsce z listy"
-                className="form-control"
-                onChange={changeHandler}
-                value={daneOsobowe.miejscowosc}
-                options={miasta}
-            />
-            <MyInput
-                type="checkbox"
-                name="dojazd"
-                label="Proszę zaznaczyć w przypadku własnego transportu"
-                className="form-control"
-                onChange={changeHandler}
-                value={daneOsobowe.dojazd}
-            />
-            <p>Proszę zaznaczyć sposób wyżywienia: </p>
-            {sposobWyzywienia.map((item, index) => (
-                <MyInput
-                    type="radio"
-                    name="sposobWyzywienia"
-                    label={item}
+        <main>
+            {daneOsobowe.miejscowosc === "Warszawa" && (
+                <img src="images/warszawa.jpg" />
+            ) || daneOsobowe.miejscowosc === "Kraków" && (
+                <img src="images/krakow.jpg" />
+            ) || daneOsobowe.miejscowosc === "Lublin" && (
+                <img src="images/lublin.jpg" />
+            ) || (
+                <img src="images/mapa_polski.jpg" />
+            )}
+            <form onSubmit={submitHandler}>
+                <MySelect
+                    name="miejscowosc"
+                    label="Proszę wybrać miejsce z listy"
                     className="form-control"
-                    value={item}
                     onChange={changeHandler}
-                    cheched={daneOsobowe.wyzywienie}
+                    value={daneOsobowe.miejscowosc}
+                    options={miasta}
                 />
-            ))}
-            <MyTextArea
-                type="text"
-                name="uwagi"
-                label="Uwagi do organizatora"
-                className="form-control"
-                value={daneOsobowe.uwagi}
-                onChange={changeHandler}
-            />
-            <MyInput
-                type="text"
-                name="email"
-                label="Adres do wysłania oferty"
-                className="form-control"
-                value={daneOsobowe.email}
-                onChange={changeHandler}
-            />
-            <MyInput
-                type="submit"
-                name="wyslij"
-                label="Wyślij zamówienie"
-                className="form-control"
-            />
-        </form>
+                <MyInput
+                    type="text"
+                    name="iloscOsob"
+                    label="Ilość osób"
+                    className="form-control"
+                    onChange={changeHandler}
+                    value={daneOsobowe.iloscOsob}
+                />
+                <MyInput
+                    type="checkbox"
+                    name="dojazdWlasny"
+                    label="Proszę zaznaczyć w przypadku własnego transportu"
+                    className="form-control"
+                    onChange={changeHandler}
+                    value={daneOsobowe.dojazdWlasny}
+                />
+                <p>Proszę zaznaczyć sposób wyżywienia: </p>
+                {sposobWyzywienia.map((item, index) => (
+                    <MyInput
+                        type="radio"
+                        name="wyzywienie"
+                        label={item}
+                        className="form-control"
+                        value={item}
+                        onChange={changeHandler}
+                        checked={daneOsobowe.wyzywienie}
+                    />
+                ))}
+                <MyTextArea
+                    type="text"
+                    name="uwagi"
+                    label="Uwagi do organizatora"
+                    className="form-control"
+                    value={daneOsobowe.uwagi}
+                    onChange={changeHandler}
+                />
+                <MyInput
+                    type="email"
+                    name="email"
+                    label="Adres do wysłania oferty"
+                    className="form-control"
+                    value={daneOsobowe.email}
+                    onChange={changeHandler}
+                />
+                <MyInput
+                    type="submit"
+                    name="wyslij"
+                    label="Wyślij zamówienie"
+                    className="form-control"
+                />
+                <p>Miejscowosc: {daneOsobowe.miejscowosc === "" ? "jeszcze nie wybrane" : daneOsobowe.miejscowosc}</p>
+                <p>Ilość osób: {isNaN(daneOsobowe.iloscOsob) || daneOsobowe.iloscOsob <= 0 ? "brak danych" : daneOsobowe.iloscOsob}</p>
+                <p>Dojazd: {daneOsobowe.dojazdWlasny ? "własny" : "organizator"}</p>
+                <p>Wyżywienie: {daneOsobowe.wyzywienie === "" ? "brak danych" : daneOsobowe.wyzywienie}</p>
+                <p>Uwagi: {daneOsobowe.uwagi === "" ? "brak" : daneOsobowe.uwagi}</p>
+                <p>Ilość osób: {daneOsobowe.email === "" ? "nie został podany" : daneOsobowe.email}</p>
+                <p>Aktualna wartość wycieczki: {daneOsobowe.wartoscWycieczki}</p>
+            </form>
+        </main>
+
     )
 }
 
